1f38d681240469a572abec37598c666363c4c57d
Oleksandr Gneushev <galexing@gmail.com>
2022-10-27 12:42:02 +0200

    Update error message

--- components/ErrorSearch.jsx
+++ components/ErrorSearch.jsx
@@ -2,12 +2,12 @@ const ErrorSearch = () => {
   return (
     <p className='grow h-full flex flex-col justify-center items-center text-center text-gray-400 text-[1.5rem] max-w-[60%] min-w-max'>
       <span>
-        Oops, something went wrong
+        Oops, the address is wrong
         <span role='img' aria-level='thinking icon'>
           🤔
         </span>
       </span>
-      Check if the address is correct!
+      or You have no NFTs!
     </p>
   );
 };
